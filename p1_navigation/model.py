cf85259b6f6e727b33e0ed608f2b2708e2f81bf6
Neilkunal Panchal <neilkunal.panchal@shell.com>
2019-02-03 22:02:33 -0600

    First Commit of p1 Deep RL nano Degree

---
+++ p1_navigation/model.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+    
+    
+class QNetworkVis(nn.Module):
+    """Actor (Policy) Model."""
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(QNetworkVis, self).__init__()
+        nfilters = [64, 128]
+        self.seed = torch.manual_seed(seed)
+        self.conv1 = nn.Conv3d(3, nfilters[0], kernel_size=(1, 5, 5), stride=(1,3,3))
+        self.bn1 = nn.BatchNorm3d(nfilters[0])
+        self.conv2 = nn.Conv3d(nfilters[0], nfilters[1], kernel_size=(1, 3, 3), stride=(1,3,3))
+        self.bn2 = nn.BatchNorm3d(nfilters[1])
+
+        conv_out_size = self._get_conv_out_size(state_size)
+        fc = [conv_out_size, 32]
+        self.fc1 = nn.Linear(fc[0], fc[1])
+        self.fc2 = nn.Linear(fc[1], action_size)
+
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = self._cnn(state)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+    def _get_conv_out_size(self, shape):
+        x = torch.rand(shape)
+        x = self._cnn(x)
+        n_size = x.data.view(1, -1).size(1)
+        print('Convolution output size:', n_size)
+        return n_size
+
+    def _cnn(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        return x
+
+
+ 
